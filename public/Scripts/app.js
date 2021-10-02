bffbdde57fab5fa435777d4e10cca43193682977
Shahrzad Kolahdooz <shahrzad.kolahdooz@gmail.com>
2021-10-02 16:28:19 -0400

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,7 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+    function Start(){
+        console.log("App Started...");
+    }
+    window.addEventListener("load", Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
+module.exports = router;
